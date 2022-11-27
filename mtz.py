364edf5455a9c76f50348997af4f7251642f1199
geneus003 <geneus003@gmail.com>
2022-11-27 21:14:20 +0300

    Added four new function's, some fixes

--- mtz.py
+++ mtz.py
@@ -1,5 +1,5 @@
-import matplotlib.pyplot as plt
 import numpy as np
+import math
 
 
 class ShowData:
@@ -24,6 +24,53 @@ class ShowData:
 
         self.position_y = np.arange(len(self.list_x))
         self.position_z = np.arange(len(self.list_y))
+
+
+class RhoSeem:
+    def __init__(self, file_path):
+        self.file = open(file_path, 'r')
+
+        self.mas_n = list(map(int, self.file.readline().split()))
+        self.h_z = list(map(int, self.file.readline().split()))
+        self.h_y = list(map(int, self.file.readline().split()))
+        self.rho = np.empty((len(self.h_y), len(self.h_z)), dtype=float)
+
+        for i in range(len(self.h_z)):
+            self.rho[i] = list(map(int, self.file.readline().split()))  # h_z layers down counter
+
+        self.n = self.mas_n[0]  # Count of layers, that a going down in table
+        self.m = 5  # count of periods
+        self.t1 = 0.01  # first period
+        self.q = 2  # period change coefficient
+
+        self.row_solutions = []
+        self.phi_solutions = []
+
+        for i in range(self.mas_n[1]):
+            temp = self.mtz_first([j[i] for j in self.rho], self.h_z)
+            self.row_solutions.append(temp[0])
+            self.phi_solutions.append(temp[1])
+
+    def mtz_first(self, p, h) -> list:
+        p, h = np.array(p), np.array(h)
+        t = np.array([self.q ** i * self.t1 for i in range(self.m)])
+        w = 2 * np.pi / t
+        u = 4 * np.pi * 10 ** (-7)
+
+        r = np.empty(self.m, dtype=complex)
+        for i, e in enumerate(w):
+            rc = 1
+            for j in reversed(range(self.n - 1)):
+                k = np.sqrt((-1 * complex("0+1j") * e * u) / p[j])
+                a = np.sqrt(p[j] / p[j + 1])
+                b = np.exp(-2 * k * h[j]) * (rc - a) / (rc + a)
+                rc = (1 + b) / (1 - b)
+            r[i] = rc
+
+        rc = np.abs(r) ** 2 * p[0]
+        phi_t = np.angle(r) - np.pi / 4
+        phi = list(map(math.degrees, (np.angle(r) - np.pi / 4)))
+        return [rc, phi]
 
 
 if __name__ == "__main__":
--- visual_data.py
+++ visual_data.py
@@ -1,10 +1,11 @@
 import matplotlib
 import matplotlib.pyplot as plt
+import numpy as np
 from PyQt5 import QtCore
 from PyQt5.QtWidgets import QWidget, QPushButton, QVBoxLayout, QGridLayout, QMessageBox
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 from matplotlib.backends.backend_qt5agg import NavigationToolbar2QT as NavigationToolbar
-from mtz import ShowData
+from mtz import ShowData, RhoSeem
 
 matplotlib.use('Qt5Agg')
 
@@ -15,6 +16,7 @@ class VisualMTZ(QWidget):
         super(VisualMTZ, self).__init__()
 
         self.file_path = file_path
+        self.data_analysed = RhoSeem(self.file_path)
         print(self.file_path)
 
         self.configure_window()  # Method to configure window parameters
@@ -25,10 +27,11 @@ class VisualMTZ(QWidget):
         self.graph_layout = QVBoxLayout()
 
         # Defining menu_layout variables
-        self.btn1 = QPushButton("Визуализировать 1", self)
-        self.btn2 = QPushButton("Визуализировать 2", self)
-        self.btn3 = QPushButton("Визуализировать 3", self)
-        self.btn4 = QPushButton("Визуализировать 4", self)
+        self.btn1 = QPushButton("Визуализировать данные", self)
+        self.btn2 = QPushButton("Визуализировать кривые rho кажущегося", self)
+        self.btn3 = QPushButton("Визуализировать карту rho кажущегося", self)
+        self.btn4 = QPushButton("Визуализировать кривые phi кажущегося", self)
+        self.btn5 = QPushButton("Визуализировать карту phi кажущегося", self)
 
         self.create_menu_layout()  # method to configure menu_layout dependencies
 
@@ -39,7 +42,6 @@ class VisualMTZ(QWidget):
         self.cb = None
 
         self.create_graph_layout()  # method to configure graph_layout dependencies
-
         self.layouts_configure()  # Method to configure layouts
 
         self.show()
@@ -54,19 +56,23 @@ class VisualMTZ(QWidget):
         self.btn1.clicked.connect(self.visual_mtz_file)
         self.btn1.setFixedHeight(35)
 
-        self.btn2.clicked.connect(self.visual_mtz_file)
+        self.btn2.clicked.connect(lambda: self.visual_rho(True))
         self.btn2.setFixedHeight(35)
 
-        self.btn3.clicked.connect(self.visual_mtz_file)
+        self.btn3.clicked.connect(lambda: self.visual_rho(False))
         self.btn3.setFixedHeight(35)
 
-        self.btn4.clicked.connect(self.visual_mtz_file)
+        self.btn4.clicked.connect(lambda: self.visual_phi(True))
         self.btn4.setFixedHeight(35)
+
+        self.btn5.clicked.connect(lambda: self.visual_phi(False))
+        self.btn5.setFixedHeight(35)
 
         self.menu_layout.addWidget(self.btn1, 0, 0, 1, 2, QtCore.Qt.AlignHCenter)
         self.menu_layout.addWidget(self.btn2, 0, 1, 1, 2, QtCore.Qt.AlignHCenter)
         self.menu_layout.addWidget(self.btn3, 0, 2, 1, 2, QtCore.Qt.AlignHCenter)
         self.menu_layout.addWidget(self.btn4, 0, 3, 1, 2, QtCore.Qt.AlignHCenter)
+        self.menu_layout.addWidget(self.btn5, 0, 4, 1, 2, QtCore.Qt.AlignHCenter)
 
     # Method to configure graph_layout vars and their alignments
     def create_graph_layout(self):
@@ -90,6 +96,9 @@ class VisualMTZ(QWidget):
         except Exception as err:
             print(err)
 
+    def canvas_update(self):
+        self.axs.clear()  # clear drawing axes
+
     # Method to visualisation mtz file
     def visual_mtz_file(self):
         if self.cb:
@@ -109,6 +118,48 @@ class VisualMTZ(QWidget):
         p2 = self.axs.imshow(file_data.rho, cmap='jet', aspect='auto', interpolation='bilinear', origin="upper")
         self.cb = self.figure.colorbar(p2, ax=self.axs)
         self.figure.canvas.draw()
+
+    # Rho seem visual
+    def visual_rho(self, is_graph):
+        self.canvas_update()
+        if is_graph:
+            self.axs.plot([i+1 for i in range(len(self.data_analysed.row_solutions))], self.data_analysed.row_solutions)
+            self.axs.set_xticks([i+1 for i in range(len(self.data_analysed.row_solutions))])
+            self.axs.set_xticklabels([i+1 for i in range(len(self.data_analysed.row_solutions))])
+            self.axs.set_xlim([1, len(self.data_analysed.row_solutions)])
+            legend_marks = [self.data_analysed.t1 * 2**(i+1) for i in range(len(self.data_analysed.row_solutions[0]))]
+            self.axs.legend(legend_marks, title="Периоды")
+            self.figure.canvas.draw()
+        else:
+            self.axs.plot([i+1 for i in range(len(self.data_analysed.row_solutions))], self.data_analysed.row_solutions)
+            self.axs.set_xticks([i+1 for i in range(len(self.data_analysed.row_solutions))])
+            self.axs.set_xticklabels([i+1 for i in range(len(self.data_analysed.row_solutions))])
+            self.axs.set_xlim([1, len(self.data_analysed.row_solutions)])
+            temp_mas = np.array(self.data_analysed.row_solutions).transpose()
+            p2 = self.axs.imshow(temp_mas, cmap='jet', aspect='auto', interpolation='bilinear', origin="upper")
+            self.cb = self.figure.colorbar(p2, ax=self.axs)
+            self.figure.canvas.draw()
+
+    # Phi visual
+    def visual_phi(self, is_graph=True):
+        self.canvas_update()
+        if is_graph:
+            self.axs.plot([i+1 for i in range(len(self.data_analysed.phi_solutions))], self.data_analysed.phi_solutions)
+            self.axs.set_xticks([i+1 for i in range(len(self.data_analysed.phi_solutions))])
+            self.axs.set_xticklabels([i+1 for i in range(len(self.data_analysed.phi_solutions))])
+            self.axs.set_xlim([1, len(self.data_analysed.phi_solutions)])
+            legend_marks = [self.data_analysed.t1 * 2**(i+1) for i in range(len(self.data_analysed.phi_solutions[0]))]
+            self.axs.legend(legend_marks, title="Периоды")
+            self.figure.canvas.draw()
+        else:
+            self.axs.plot([i+1 for i in range(len(self.data_analysed.phi_solutions))], self.data_analysed.phi_solutions)
+            self.axs.set_xticks([i+1 for i in range(len(self.data_analysed.phi_solutions))])
+            self.axs.set_xticklabels([i+1 for i in range(len(self.data_analysed.phi_solutions))])
+            self.axs.set_xlim([1, len(self.data_analysed.phi_solutions)])
+            temp_mas = np.array(self.data_analysed.phi_solutions).transpose()
+            p2 = self.axs.imshow(temp_mas, cmap='jet', aspect='auto', interpolation='bilinear', origin="upper")
+            self.cb = self.figure.colorbar(p2, ax=self.axs)
+            self.figure.canvas.draw()
 
     def error_window(self):
         pass
